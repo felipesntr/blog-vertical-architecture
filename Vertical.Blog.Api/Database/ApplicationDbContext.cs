05dcf314de166659460872cd862096f4edc5a007
Felipe Santos <rfelipe273@gmail.com>
2024-02-10 00:57:22 -0300

    Add project files.

---
+++ Vertical.Blog.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vertical.Blog.Api.Entities;
+
+namespace Vertical.Blog.Api.Database;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Article>(builder =>
+            builder.OwnsOne(a => a.Tags, tagsBuilder => tagsBuilder.ToJson()));
+    }
+
+    public DbSet<Article> Articles { get; set; }
+}
---
+++ Vertical.Blog.Api/Features/Articles/CreateArticle.cs
@@ -0,0 +1,11 @@
+﻿namespace Vertical.Blog.Api.Features.Articles;
+
+public static class CreateArticle
+{
+    public class Command
+    { 
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public List<string> Tags { get; set; } = [];
+    }
+}
